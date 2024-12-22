37b1d234c9a07ccc3c4ae33e1679a0f2f3fd5df0
dean <deanfernandes18@gmail.com>
2024-12-22 16:27:58 +0000

    add emugu pkg; add imageprocessor

---
+++ DeanFernandes.Dota2UltOverlay/DeanFernandes.Dota2UltOverlay/ImageProcessor.cs
@@ -0,0 +1,27 @@
+﻿using Emgu.CV.CvEnum;
+using Emgu.CV;
+using System.Diagnostics;
+using System.Drawing;
+using Emgu.CV.Structure;
+using System.Reflection;
+
+namespace DeanFernandes.Dota2UltOverlay
+{
+    public class ImageProcessor
+    {
+        private const double MatchThreshold = 0.7;
+        public static bool PerformTemplateMatch(string imageAPath, string imageBPath)
+        {
+            using Mat imageA = CvInvoke.Imread(imageAPath, Emgu.CV.CvEnum.ImreadModes.Color);
+            using Mat imageB = CvInvoke.Imread(imageBPath, Emgu.CV.CvEnum.ImreadModes.Color);
+
+            Mat result = new Mat();
+            CvInvoke.MatchTemplate(imageA, imageB, result, Emgu.CV.CvEnum.TemplateMatchingType.CcorrNormed);
+            double minVal = 0, maxVal = 0;
+            Point minLoc = default, maxLoc = default;
+            CvInvoke.MinMaxLoc(result, ref minVal, ref maxVal, ref minLoc, ref maxLoc);
+
+            return maxVal >= MatchThreshold;
+        }
+    }
+}
